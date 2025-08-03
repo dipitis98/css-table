63af6048e80b184be775411bb06f012a02a7a317
Dip Paul <dip1221paul@gmail.com>
2025-08-03 23:29:41 +0600

    added script.js

---
+++ script.js
@@ -0,0 +1,81 @@
+// Array methods                               
+
+// let arr = ['dip', 'dola', 'priya', 'tanmay']
+// console.log(arr.length);
+
+// baad = arr.pop()
+// console.log(baad);
+
+// arr.push('sunny')
+// console.log(arr);
+
+// prothomerjonBaad = arr.shift()
+// console.log(prothomerjonBaad);
+
+// arr.unshift('nahid')
+// console.log(arr);
+
+// Multi-dimensional Array
+
+// let arr = [
+//     ['dip','ctg',26],
+//     ['dola','ctg',22], 
+//     ['priya', 'ctg', 23],
+//     ['tanmay','dhaka',12,['reading','gossiping']],
+// ]
+
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[3][3][1]);
+
+// ODD-EVEN Checker
+// let x = 
+// if (x % 2 == 0) {
+//     console.log('The number that you have entered is EVEN.');
+// } else{
+//     console.log("The number that you have entered is ODD.");
+// }
+
+// Positive-Negative Checker
+// let a = 
+// if (a > 0) {
+//     console.log("The number is positive.");
+// } else {
+//     console.log("The number is negative.");
+// }
+
+// Adult Checker
+
+// let age = 10
+// if (age < 18) {
+//     console.log("You are not allowed to visit this site.");
+// } else {
+//     console.log("You are allowed to visit this site.");   
+// }
+
+// Grade Calculator
+// let number = 22
+
+// if (number >=80) {
+//     console.log("A+");
+// } else if (number >=70) {
+//     console.log("A")
+// } else if (number >=60) {
+//     console.log("A-")
+// } else if (number >=50) {
+//     console.log("B")
+// } else if (number >=40) {
+//     console.log("C")
+// } else if (number >=33) {
+//     console.log("D");
+// } else {
+//     console.log("Fail")
+// }
+
+// Leap Year Checker 
+// let year = 2024
+// if (year % 4 == 0 && year % 100 !== 0 || year % 400 == 0) {
+//     console.log("This is a leap year.")
+// } else {
+//     console.log("This is not a leap year.")
+// }
